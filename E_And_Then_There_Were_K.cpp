ca53381bdb432f27c88f20b1893a01767800f1aa
AdnanTorofdar <160164853+AdnanTorofdar@users.noreply.github.com>
2024-04-18 23:13:05 +0600

    Add files via upload

---
+++ E_And_Then_There_Were_K.cpp
@@ -0,0 +1,35 @@
+//-----------_The_eRRoR_----------
+//---------Adnan_Torofdar----------
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define nl '\n'
+#define fast                      \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+#define mod 1000000007
+void _The_eRRoR_()
+{
+    int n;
+    cin >> n;
+    for (int i = 31; i >= 0; i--)
+    {
+        if (n & (1 << i))
+        {
+            cout << ((1 << i) - 1) << "\n";
+            break;
+        }
+    }
+}
+int main()
+{
+    fast;
+    ll test_case = 1;
+    cin >> test_case;
+    while (test_case--)
+        _The_eRRoR_();
+    return 0;
+}
